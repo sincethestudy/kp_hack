ff752da7f9048021152264b3cb3e51850afb8d96
jiito <benjamin.allanrahill@gmail.com>
2023-07-23 13:14:53 -0700

    initial grid ui and complete funcc

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+  import { complete } from "./complete.js";
   let userPrompt = "";
+  let systemPrompt = "THIS IS THE SYSTEM PROMPT";
 
   $: promptVariables = userPrompt
     .match(/\{(.*?)\}/g)
@@ -7,75 +9,41 @@
   $: console.log(promptVariables);
   $: dataVariables = [];
 
+  let gridArray = ["", "", "", ""];
+
   async function sendPrompt() {
-    console.log(userPrompt);
-    for (let i = 0; i < promptVariables.length; i++) {
-      console.log(promptVariables[i], dataVariables[i]);
-    }
-    await complete();
+    await complete(gridArray, userPrompt, systemPrompt, promptVariables);
   }
-
-  async function complete() {
-    const response = await fetch("http://localhost:8000/complete", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        system_message: "You are an animal generator",
-        user_message: userPrompt,
-        inputs: dataVariables,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error("HTTP error " + response.status);
-    }
-
-    let reader = response.body.getReader();
-    let decoder = new TextDecoder();
-
-    streamed_response = "";
-
-    while (true) {
-      let { value, done } = await reader.read();
-      if (done) {
-        break;
-      }
-      let decoded_value = decoder.decode(value, { stream: true });
-      let lines = decoded_value.split("\n").filter(Boolean);
-
-      lines.forEach((line) => {
-        let { text, box_idx } = JSON.parse(line);
-        arr[box_idx] += text;
-        arr = [...arr];
-      });
-    }
-  }
-  let arr = ["", "", "", ""];
 </script>
 
 <h1 class="text-2xl font-bold">PromptBreeder</h1>
-<main class="flex items-start flex-col px-12 py-8 w-full">
-  <section class="w-full">
+<main class="flex items-start flex-row px-12 py-8 w-full">
+  <section class="w-1/4">
     <h3>Input Prompt</h3>
-    <input
+    <textarea
+      type="text"
       bind:value={userPrompt}
       placeholder="Insert the prompt here"
-      class="input border border-red-500 rounded w-full"
+      class="input border border-slate-500 rounded w-full"
     />
-    {promptVariables}
-    <h3>Variables</h3>
-    <input class="input border border-red-500 rounded" />
     {#if promptVariables}
       {#each promptVariables as v, i}
-        <div>Variable {v}</div>
-        <input
-          bind:value={dataVariables[i]}
-          class="input border border-red-500 rounded w-full"
-        />
+        <div class="flex flex-row items-center justify-start">
+          <span class="">{i} :</span>
+          <input
+            bind:value={dataVariables[i]}
+            class="input border border-gray-500 rounded"
+          />
+        </div>
       {/each}
     {/if}
+    <h3>Meta Prompt</h3>
+    <textarea
+      type="text"
+      bind:value={systemPrompt}
+      placeholder={systemPrompt}
+      class="input border border-slate-500 rounded w-full"
+    />
     <button
       on:click={sendPrompt}
       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -83,16 +51,12 @@
       Generate
     </button>
   </section>
-  <section>
-    {#each arr as item}
-      <p>{item}</p>
+  <section class="flex w-3/4 flex-wrap">
+    {#each gridArray as item}
+      <div class="max-w-lg bg-slate-50">{item}</div>
     {/each}
   </section>
 </main>
 
 <style>
-  .input {
-    padding: 0.5rem;
-    color: dimgrey;
-  }
 </style>
---
+++ my-app/src/routes/complete.js
@@ -0,0 +1,37 @@
+export async function complete(arr, dataVariables, systemPrompt, userPrompt) {
+  const response = await fetch("http://localhost:8000/complete", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      system_message: systemPrompt,
+      user_message: userPrompt,
+      inputs: dataVariables,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("HTTP error " + response.status);
+  }
+
+  let reader = response.body.getReader();
+  let decoder = new TextDecoder();
+
+  // streamed_response = "";
+
+  while (true) {
+    let { value, done } = await reader.read();
+    if (done) {
+      break;
+    }
+    let decoded_value = decoder.decode(value, { stream: true });
+    let lines = decoded_value.split("\n").filter(Boolean);
+
+    lines.forEach((line) => {
+      let { text, box_idx } = JSON.parse(line);
+      arr[box_idx] += text;
+      arr = [...arr];
+    });
+  }
+}

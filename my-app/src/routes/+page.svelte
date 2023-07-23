393ecd1c8eaca190efe8f2fc44ce05b103d56465
David Anyaeche <david_anyaeche@Davids-MacBook-Pro-3.local>
2023-07-23 14:49:26 -0700

    grid generating prompts

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -91,43 +91,26 @@
     </button>
   </section>
   <section class="flex w-3/4 flex-wrap">
+    <div class="container m-auto grid grid-cols-2 gap-4">
     {#each arr as item}
-      <div class="max-w-lg bg-slate-50">{item}</div>
+      <!-- <div class="max-w-lg bg-slate-50">{item}</div> -->
+        <div class="tile bg-teal-500 overflow-y-scroll">
+          <h1 class="tile-marker overflow-y-scroll">{item}</h1>
+        </div>
+        <!-- <div class="tile bg-amber-500">
+          <h1 class="tile-marker">TWO</h1>
+        </div>
+        <div class="tile bg-yellow-500">
+          <h1 class="tile-marker">THREE</h1>
+        </div>
+        <div class="tile bg-lime-600">
+          <h1 class="tile-marker">FOUR</h1>
+        </div> -->
     {/each}
+    </div>
   </section>
 
-  <div class="container m-auto grid grid-cols-2 gap-4">
-    <div class="tile bg-teal-500">
-      <h1 class="tile-marker">ONE</h1>
-    </div>
-    <div class="tile bg-amber-500">
-      <h1 class="tile-marker">TWO</h1>
-    </div>
-    <div class="tile bg-yellow-500">
-      <h1 class="tile-marker">THREE</h1>
-    </div>
-    <div class="tile bg-lime-600">
-      <h1 class="tile-marker">FOUR</h1>
-    </div>
-    <div class="tile bg-green-600">
-      <h1 class="tile-marker">FIVE</h1>
-    </div>
-    <div class="tile bg-emerald-500">
-      <h1 class="tile-marker">SIX</h1>
-    </div>
-    <div class="tile bg-teal-500">
-      <h1 class="tile-marker">SEVEN</h1>
-    </div>
-    <div class="tile bg-purple-500">
-      <h1 class="tile-marker">EIGHT</h1>
-    </div>
-    <div class="tile bg-pink-500">
-      <h1 class="tile-marker">NINE</h1>
-    </div>
-
-
-
-
+  
 </main>
 
 <style>
